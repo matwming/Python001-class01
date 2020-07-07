f4f48aac7aea98a51a085871ec6a0fea0078ea13
ming <matwming114@gmail.com>
2020-07-08 07:20:53 +1000

    added md

--- week02/assignment2-selenium/app.py
+++ week02/assignment2-selenium/app.py
@@ -16,25 +16,27 @@ def element_btns(browser):
     password = browser.find_element_by_name('password')
     login = browser.find_element_by_css_selector('.submit')
     return email, password, login
-
-try:
-    browser = webdriver.Chrome()
-    browser.get('https://shimo.im/welcome')
-    time.sleep(1)
-    login_btn = browser.find_element_by_css_selector('.login-button')
-    print(login_btn.text)
-    if login_btn:
-        login_btn.click()
-        (input_email, input_password, login_btn) = element_btns(browser)
-        input_email.send_keys('tedigix813@frost2d.net')
-        input_password.send_keys('753951')
+def main():
+    try:
+        browser = webdriver.Chrome()
+        browser.get('https://shimo.im/welcome')
         time.sleep(1)
-        login_btn.click()
-        time.sleep(3)
-        if is_login_success(browser):
-            print('logged in shimo successfully!')
-except:
-    print('an error occurred.')
-finally:
-    print('chrome is closed')
-    browser.quit()
+        login_btn = browser.find_element_by_css_selector('.login-button')
+        print(login_btn.text)
+        if login_btn:
+            login_btn.click()
+            (input_email, input_password, login_btn) = element_btns(browser)
+            input_email.send_keys('tedigix813@frost2d.net')
+            input_password.send_keys('753951')
+            time.sleep(1)
+            login_btn.click()
+            time.sleep(3)
+            if is_login_success(browser):
+                print('logged in shimo successfully!')
+    except:
+        print('an error occurred.')
+    finally:
+        print('chrome is closed')
+        browser.quit()
+
+main()
---
+++ week02/assignment2-selenium/readme.md
@@ -0,0 +1,13 @@
+## how to run this app?
+1.run in virtual environment:
+```python
+pipenv shell
+```
+2.install all dependencies and chromedriver
+```python
+pipenv install
+```
+3.run when you are in virtual environment
+```python
+python3 app.py
+```
